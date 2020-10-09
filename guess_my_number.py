17e63d3f342b64851d379105b67412f3559099bf
Thilo PALOMERAS <thilo.palomeras@epfl.ch>
2020-10-09 12:08:10 +0200

    Transformation du code avec l'ajout de la classe GuessMachine

--- guess_my_number.py
+++ guess_my_number.py
@@ -10,6 +10,19 @@ import random
 MIN=0
 MAX=1000
 
+class GuessMachine():
+    def __init__(self):
+        self._number_to_guess = random.randint(MIN,MAX)
+        self.number_of_attempt = 0
+    def guess(self,num):
+        self.number_of_attempt +=1
+        if num<self._number_to_guess:
+            return 'too low'
+        elif num>self._number_to_guess:
+            return 'too high'
+        else:
+            return 'found'
+
 if __name__== '__main__':
     number_to_guess = random.randint(MIN,MAX)
     print('Hey! Try to guess a number between %d and %d' % (MIN,MAX))
